6bb39957f366633aab6b95543241e63ce0dbe7d5
Yel0n <devqa1337@gmail.com>
2017-12-16 15:56:38 +0100

    Main metoda z random

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(PiExtension.PiRepresentative(5));
+        Random random = new Random();
+
+        for (int i = 0; i <5; i++) {
+            int r = random.nextInt(15);
+            System.out.println(PiExtension.PiRepresentative(r));
+
+        }
+
+
     }
 }
